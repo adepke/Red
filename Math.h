f3d2e87fdc772719f109dc4c94640a17aff06b00
Andrew <andrewdepke@gmail.com>
2017-05-06 12:15:07 -0600

    Added Ray, LineSegment, Box, AABB, Updated Math.h

---
+++ Math.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "../Core/Definitions.h"
+
+#include "Vector.h"
+#include "Quaternion.h"
+#include "Matrix.h"
+#include "Ray.h"
+#include "LineSegment.h"
+#include "Box.h"
+#include "AABB.h"
+
+#include "Intersection.h"
+
+namespace Red
+{
+	namespace Math
+	{
+		const Vector3 Vector3::ZeroVector(0, 0, 0);
+		const Vector3 Vector3::ForwardVector(1, 0, 0);
+		const Vector3 Vector3::RightVector(0, 1, 0);
+		const Vector3 Vector3::UpVector(0, 0, 1);
+
+		const Quaternion Quaternion::ZeroQuaternion(0, 0, 0, 0);
+		const Quaternion Quaternion::IdentityQuaternion(0, 0, 0, 1);
+
+		uint64 Factorial(uint32 Factor)
+		{
+			return (Factor == 1 ? 1 : Factor * Factorial(Factor - 1));
+		}
+	}  // namespace Math
+}  // namespace Red
