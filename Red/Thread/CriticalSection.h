63a2c0d940b6b1574cee00fe8a956d53130bada5
Andrew <andrewdepke@gmail.com>
2017-08-10 11:51:15 -0600

    Added Support for Macintosh, BSD, Android, IOS

--- Red/Thread/CriticalSection.h
+++ Red/Thread/CriticalSection.h
@@ -4,7 +4,15 @@
 
 #if OS_WINDOWS
 	#include "Windows\WindowsCriticalSection.h"
+#elif OS_MACINTOSH
+	#include "POSIX\POSIXCriticalSection.h"
 #elif OS_LINUX
+	#include "POSIX\POSIXCriticalSection.h"
+#elif OS_BSD
+	#include "POSIX\POSIXCriticalSection.h"
+#elif OS_ANDROID
+	#include "POSIX\POSIXCriticalSection.h"
+#elif OS_IOS
 	#include "POSIX\POSIXCriticalSection.h"
 #endif
 
