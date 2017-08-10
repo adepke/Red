bbeb21a432585866b9dbeafe5de42f4dad222519
Andrew <andrewdepke@gmail.com>
2017-08-10 11:32:51 -0600

    Added POSIX Implementation of Critical Section

---
+++ Red/Thread/POSIX/POSIXCriticalSection.cpp
@@ -0,0 +1,28 @@
+#include "POSIXCriticalSection.h"
+
+namespace Red
+{
+	POSIXCriticalSection::POSIXCriticalSection()
+	{
+		pthread_mutexattr_t HandleAttributes;
+		pthread_mutexattr_init(&HandleAttributes);
+		pthread_mutexattr_settype(&HandleAttributes, PTHREAD_MUTEX_RECURSIVE);
+		pthread_mutex_init(&Handle, &HandleAttributes);
+		pthread_mutexattr_destroy(&HandleAttributes);
+	}
+
+	POSIXCriticalSection::~POSIXCriticalSection()
+	{
+		pthread_mutex_destroy(&Handle);
+	}
+
+	void POSIXCriticalSection::Lock()
+	{
+		pthread_mutex_lock(&Handle);
+	}
+
+	void POSIXCriticalSection::Unlock()
+	{
+		pthread_mutex_unlock(&Handle);
+	}
+}  // namespace Red
---
+++ Red/Thread/POSIX/POSIXCriticalSection.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <pthread.h>
+
+namespace Red
+{
+	class POSIXCriticalSection
+	{
+	private:
+		pthread_mutex_t Handle;
+
+	public:
+		POSIXCriticalSection();
+		~POSIXCriticalSection();
+
+		void Lock();
+		void Unlock();
+
+	private:
+		POSIXCriticalSection(const POSIXCriticalSection& Target);
+		POSIXCriticalSection& operator=(const POSIXCriticalSection& Target);
+	};
+
+	// Private inheritance to prevent calls to Lock() and Unlock()
+	class POSIXRAIICriticalSection : private POSIXCriticalSection
+	{
+	public:
+		POSIXRAIICriticalSection()
+		{
+			POSIXCriticalSection::Lock();
+		}
+
+		~POSIXRAIICriticalSection()
+		{
+			POSIXCriticalSection::Unlock();
+		}
+	};
+
+	typedef POSIXCriticalSection CriticalSection;
+	typedef POSIXRAIICriticalSection RAIICriticalSection;
+}  // namespace Red
