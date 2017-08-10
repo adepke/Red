fd550313391a0e89ef4633ab3e9c57abc2624e84
Andrew <andrewdepke@gmail.com>
2017-08-09 20:59:00 -0600

    Added Namespace

--- Red/Math/BinaryOps.h
+++ Red/Math/BinaryOps.h
@@ -4,28 +4,31 @@
 
 // Least significant bit is 0
 
-template <typename Type>
-byte BitRead(const Type& Target, const uint32 Bit)
+namespace Red
 {
-	return (Target & (1 << Bit));
-}
-
-template <typename Type>
-void BitWrite(Type& Target, const uint32 Bit, const byte Value)
-{
-	if (Value)
+	template <typename Type>
+	byte BitRead(const Type& Target, const uint32 Bit)
 	{
-		Base |= 1 << Bit;
+		return (Target & (1 << Bit));
 	}
 
-	else
+	template <typename Type>
+	void BitWrite(Type& Target, const uint32 Bit, const byte Value)
 	{
-		Base &= ~(1 << Bit);
-	}
-}
+		if (Value)
+		{
+			Base |= 1 << Bit;
+		}
 
-template <typename Type>
-void BitFlip(Type& Target, const uint32 Bit)
-{
-	Base ^ (1 << Bit);
-}
+		else
+		{
+			Base &= ~(1 << Bit);
+		}
+	}
+
+	template <typename Type>
+	void BitFlip(Type& Target, const uint32 Bit)
+	{
+		Base ^ (1 << Bit);
+	}
+}  // namespace Red
