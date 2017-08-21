4edc49e3e918a646e3cf65e93b7ae72c0ce24166
Andrew <andrewdepke@gmail.com>
2017-08-21 09:33:40 -0600

    Added Interface Headers

---
+++ Red/Window/Window.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../Core/Platform.h"
+
+#if OS_WINDOWS
+	#include "Windows\WindowsWindow.h"
+#endif
+
+// Window is a typedef to a platform specific implementation
---
+++ Red/Window/WindowUtils.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "../Core/Platform.h"
+
+#if OS_WINDOWS
+	#include "Windows/WindowsWindowUtils.h"
+#endif
+
+// WindowDescription is a typedef to a platform specific implementation.
+// WindowState is a typedef to a platform specific implementation.
