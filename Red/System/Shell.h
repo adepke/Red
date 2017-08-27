ac7bb12b12f977268266d0ffea6eaf7e4d7f8043
Andrew Depke <andrewdepke@gmail.com>
2017-08-27 01:21:30 -0600

    Added System Module, Shell Command Executor

---
+++ Red/System/Shell.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "../Core/Platform.h"
+
+#include <string>
+#include <array>
+#include <stdexcept>
+#include <memory>
+
+// Execute Command Through Shell
+std::string ShellExec(const char* Command)
+{
+	std::array<char, 128> Buffer;
+	std::string Result;
+
+#if OS_WINDOWS
+	FILE* Pipe = _popen(Command, "r");
+#else
+	FILE* Pipe = popen(Command, "r");
+#endif
+	if (!Pipe)
+	{
+		throw std::runtime_error("Failed to Create Shell Command Pipe");
+	}
+
+	while (!feof(Pipe))
+	{
+		if (fgets(Buffer.data(), 128, Pipe))
+		{
+			Result += Buffer.data();
+		}
+	}
+
+#if OS_WINDOWS
+	_pclose(Pipe);
+#else
+	pclose(Pipe);
+#endif
+	
+	return Result;
+}
