9c7f7427c3a82812696948d6ac42751758359dc2
Andrew Depke <andrewdepke@gmail.com>
2018-01-24 21:08:46 -0700

    Work on Gauss-Jordan Eliminator

--- Red/Algorithm/GaussJordanEliminator.h
+++ Red/Algorithm/GaussJordanEliminator.h
@@ -41,6 +41,45 @@ namespace Red
 				Target.Data[RowB][Iter] = Swappable[Iter];
 			}
 		}
+
+		template <int Rows, int Columns>
+		void DivideRow(Matrix<Rows, Columns>& Target, int Row, float Value)
+		{
+#ifdef ASSERTIONS
+			assert(Value != 0.f);
+#endif
+
+			for (int Iter = 0; Iter < Columns; ++Iter)
+			{
+				Target.Data[Row][Iter] /= Value;
+			}
+		}
+
+		template <int Rows, int Columns>
+		void MultiplyRowCopy(const Matrix<Rows, Columns>& Source, float* Output, int Row, float Value)
+		{
+#ifdef ASSERTIONS
+			assert(Output);
+#endif
+
+			for (int Iter = 0; Iter < Columns; ++Iter)
+			{
+				Output[Iter] = Source.Data[Row][Iter] * Value;
+			}
+		}
+
+		template <int Rows, int Columns>
+		void AddArrayToRow(Matrix<Rows, Columns>& Target, float* Array, int Row)
+		{
+#ifdef ASSERTIONS
+			assert(Array);
+#endif
+
+			for (int Iter = 0; Iter < Columns; ++Iter)
+			{
+				Target.Data[Row][Iter] += Array[Iter];
+			}
+		}
 	}  // namespace Internal
 
 	template <int Rows, int Columns>
@@ -83,6 +122,71 @@ namespace Red
 
 			// Elements before NewStartIndex are in the correct position, add 1 to the minimum index range.
 			StartIndex = NewStartIndex + 1;
+		}
+
+		for (int Row = 0; Row < Rows; ++Row)
+		{
+			// Get the Next Pivot Column.
+			int PivotColumn = Internal::RowZeroCount(Result, Row);
+
+			// If this Row is All Zeros, Move on.
+			if (PivotColumn == Columns)
+			{
+				break;
+			}
+
+			// If we are on the Last Row of the Matrix.
+			if (Row == Rows)
+			{
+				if (Result.Data[Row][PivotColumn] != 1.0f)
+				{
+					// Make the Pivot 1.
+					Internal::DivideRow(Result, Row, Result.Data[Row][PivotColumn]);
+				}
+
+				break;
+			}
+
+			// Check to See if this is an Isolated Row.
+			if (Result.Data[Row + 1][PivotColumn] != 0.f)
+			{
+				if (Result.Data[Row][PivotColumn] != 1.0f)
+				{
+					// Make the Pivot 1.
+					Internal::DivideRow(Result, Row, Result.Data[Row][PivotColumn]);
+				}
+
+				// The Additive Row.
+				float ResultRow[Columns];
+
+				Internal::MultiplyRowCopy(Result, ResultRow, Row, Result.Data[Row + 1][PivotColumn]);
+
+				// Make Sure The Pivot is the Opposite Polarity as the Leading Row's.
+				if (ResultRow[PivotColumn] > 0.f)
+				{
+					if (Result.Data[Row + 1][PivotColumn] > 0.f)
+					{
+						for (int Iter = 0; Iter < Columns; ++Iter)
+						{
+							ResultRow[Iter] *= 1.0f;
+						}
+					}
+				}
+
+				else
+				{
+					if (Result.Data[Row + 1][PivotColumn] < 0.f)
+					{
+						for (int Iter = 0; Iter < Columns; ++Iter)
+						{
+							ResultRow[Iter] *= 1.0f;
+						}
+					}
+				}
+
+				// Add the Additive Row and the Leading Row Together to Cancel Out the Pivot's Lead, Leaving an Isolated Row.
+				Internal::AddArrayToRow(Result, ResultRow, Row);
+			}
 		}
 
 		return Result;
