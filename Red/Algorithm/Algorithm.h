b74d4cf27c90fc310a02d5dc9c0f01427ad83571
Andrew Depke <andrewdepke@gmail.com>
2018-01-24 19:58:32 -0700

    Fixed Swappable Compile Error

--- Red/Algorithm/Algorithm.h
+++ Red/Algorithm/Algorithm.h
@@ -1,3 +1,4 @@
 #pragma once
 
 #include "Distribute.h"
+#include "GaussJordanEliminator.h"
--- Red/Algorithm/GaussJordanEliminator.h
+++ Red/Algorithm/GaussJordanEliminator.h
@@ -1,1 +1,100 @@
 #pragma once
+
+#include "../Math/Matrix.h"
+
+namespace Red
+{
+	namespace Internal
+	{
+		template <int Rows, int Columns>
+		int RowZeroCount(const Matrix<Rows, Columns>& Target, int Row)
+		{
+			int Result = 0;
+
+			for (int Iter = 0; Iter < Columns; ++Iter)
+			{
+				if (Target.Data[Row][Iter] == 0.f)
+				{
+					++Result;
+				}
+
+				else
+				{
+					break;
+				}
+			}
+
+			return Result;
+		}
+
+		template <int Rows, int Columns>
+		void SwapRow(Matrix<Rows, Columns>& Target, int RowA, int RowB)
+		{
+			float Swappable[Columns];
+
+			for (int Iter = 0; Iter < Columns; ++Iter)
+			{
+				Swappable[Iter] = Target.Data[RowA][Iter];
+
+				Target.Data[RowA][Iter] = Target.Data[RowB][Iter];
+
+				Target.Data[RowB][Iter] = Swappable[Iter];
+			}
+		}
+	}  // namespace Internal
+
+	template <int Rows, int Columns>
+	Matrix<Rows, Columns> RowEchelon(const Matrix<Rows, Columns>& Target)
+	{
+		Matrix<Rows, Columns> Result = Target;
+
+		// Use Cocktail Shaker Sort
+
+		int StartIndex = 1;
+		int EndIndex = Rows - 1;
+
+		while (StartIndex <= EndIndex)
+		{
+			int NewStartIndex = EndIndex;
+			int NewEndIndex = StartIndex;
+
+			for (int Iter = StartIndex; Iter < EndIndex; ++Iter)
+			{
+				if (Internal::RowZeroCount(Target, Iter) > Internal::RowZeroCount(Target, Iter + 1))
+				{
+					Internal::SwapRow(Result, Iter, Iter + 1);
+
+					NewEndIndex = Iter;
+				}
+			}
+
+			// Elements after NewEndIndex are in the correct position, subtract 1 from the max index range.
+			EndIndex = NewEndIndex - 1;
+
+			for (int Iter = EndIndex; Iter > StartIndex; --Iter)
+			{
+				if (Internal::RowZeroCount(Target, Iter) > Internal::RowZeroCount(Target, Iter + 1))
+				{
+					Internal::SwapRow(Result, Iter, Iter + 1);
+
+					NewStartIndex = Iter;
+				}
+			}
+
+			// Elements before NewStartIndex are in the correct position, add 1 to the minimum index range.
+			StartIndex = NewStartIndex + 1;
+		}
+
+		return Result;
+	}
+
+	template <int Rows, int Columns>
+	Matrix<Rows, Columns> ReducedRowEchelon(const Matrix<Rows, Columns>& Target)
+	{
+		Matrix<Rows, Columns> Result;
+
+		Result = RowEchelon(Target);
+
+		return Result;
+	}
+}  // namespace Red
