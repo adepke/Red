4f26a00c66f61f9da8f0b785d95f1ee4e6b57808
Andrew <andrewdepke@gmail.com>
2018-01-30 12:50:51 -0700

    Testing Automatic VS 2017 Install Directory Detection

--- Build.bat
+++ Build.bat
@@ -1,4 +1,16 @@
 @ECHO OFF
 
-CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+IF NOT EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" (
+	ECHO "An installion of Microsoft Visual Studio 2017 version 15.2 or later does not exist."
+	PAUSE
+  EXIT /B
+)
+
+FOR /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) DO (
+  SET VSINSTALLPATH=%%i
+)
+
+CALL "!VSINSTALLPATH!\Common7\Tools\VsDevCmd.bat"
 MSBuild.exe "%~dp0\Red.sln" /t:Build /p:Configuration=Release /p:Platform="x64" /nologo
