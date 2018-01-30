8c0066225ceaf11d5a9a64a2bb37b2f24f40c859
Andrew <andrewdepke@gmail.com>
2018-01-30 12:32:01 -0700

    Added Automatic Solution Specifier

--- Build.bat
+++ Build.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
-MSBuild.exe "D:\Code\Projects\Red\Red.sln" /t:Build /p:Configuration=Release /p:Platform="x64" /nologo
+MSBuild.exe "%~dp0\Red.sln" /t:Build /p:Configuration=Release /p:Platform="x64" /nologo
