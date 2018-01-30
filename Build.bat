672189fc378290ff8a2b4d21ea839512444d62fb
Andrew <andrewdepke@gmail.com>
2018-01-29 19:35:30 -0700

    Windows Build Script Progress, Need to Prevent MSVS from Booting on Execution.

--- Build.bat
+++ Build.bat
@@ -1,9 +1,4 @@
 @ECHO OFF
-where /q msbuild.exe
-IF ERRORLEVEL 1 (
-	ECHO MSBuild was not found in your system PATH. Please install Visual Studio or add MSBuild.exe to the system PATH if it already exists.
-	PAUSE
-) ELSE (
-	msbuild Red.sln /t:Build /p:Configuration=Release /p:Platform="x64"
-	PAUSE
-)
+
+set MSBUILDPATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin"
+start %MSBUILDPATH%\MSBuild.exe Red.sln /t:Build /p:Configuration=Release /p:Platform="x64" /nologo
