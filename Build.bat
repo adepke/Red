8e4deb863dcdffe4a9754289078a5b6b6e45560a
Andrew <andrewdepke@gmail.com>
2018-01-29 23:23:51 -0700

    Implemented Working Windows Build Script, Need to Utilize Dynamic Directory Structure

--- Build.bat
+++ Build.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 
-set MSBUILDPATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin"
-start %MSBUILDPATH%\MSBuild.exe Red.sln /t:Build /p:Configuration=Release /p:Platform="x64" /nologo
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+MSBuild.exe "D:\Code\Projects\Red\Red.sln" /t:Build /p:Configuration=Release /p:Platform="x64" /nologo
