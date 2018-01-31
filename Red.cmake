81a9a7ea8aea9820628932779565da6b6d3fb470
Andrew <andrewdepke@gmail.com>
2018-01-31 11:29:14 -0700

    Added CMake Project File

---
+++ Red.cmake
@@ -0,0 +1,14 @@
+#
+# This file was genared by proj2cmake and will be overwritten on it's next run!
+# Please put all configurations in the cmake_conf/*.cmake files.
+#
+
+SET(Red_SRC
+    "Red/Hardware/Linux/LinuxHardware.cpp"
+    "Red/Network/BSD/BSDSocket.cpp"
+    "Red/Network/BSD/BSDSocketSubsystem.cpp"
+    "Red/Thread/POSIX/POSIXCriticalSection.cpp"
+   )
+
+SET(Red_DEPS
+   )
