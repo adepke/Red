ab2929568eb6f0428a223b69f2e3f249d0e7a2eb
Andrew <andrewdepke@gmail.com>
2018-02-01 22:57:43 -0700

    Removed Boilerplate

--- Red.cmake
+++ Red.cmake
@@ -1,8 +1,3 @@
-#
-# This file was genared by proj2cmake and will be overwritten on it's next run!
-# Please put all configurations in the cmake_conf/*.cmake files.
-#
-
 SET(Red_SRC
     "Red/Hardware/Linux/LinuxHardware.cpp"
     "Red/Network/BSD/BSDSocket.cpp"
