c0c397f71c1d61febe4ffa35dac799d0db7c930b
Andrew Depke <andrewdepke@gmail.com>
2018-02-23 16:50:16 -0700

    Removed Red CMake Module

--- Red.cmake
+++
@@ -1,9 +0,0 @@
-SET(Red_SRC
-    "Red/Hardware/Linux/LinuxHardware.cpp"
-    "Red/Network/BSD/BSDSocket.cpp"
-    "Red/Network/BSD/BSDSocketSubsystem.cpp"
-    "Red/Thread/POSIX/POSIXCriticalSection.cpp"
-   )
-
-SET(Red_DEPS
-   )
