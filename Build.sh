25017fad751576b95d610557d93b3c315b22a3da
XenonicDev <andrewdepke@gmail.com>
2018-02-24 22:08:51 -0700

    Added Configuration and Platform Commandline Args for Unix Builds

--- Build.sh
+++ Build.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+if [ "$1" == "Release" -o "$1" == "Debug" ]; then
+	Arguments="-DCMAKE_BUILD_TYPE=$1"
+else
+	Arguments="-DCMAKE_BUILD_TYPE=Release"
+fi
+
+if [ "$2" == "x86" -o "$2" == "x64" ]; then
+	Arguments="${Arguments} -DPLATFORM:STRING=\"$2\""
+else
+	Arguments="${Arguments} -DPLATFORM:STRING=\"x64\""
+fi
+
+if [ "$3" == "coverage" ]; then
+	Arguments="${Arguments} -DCOVERAGE=ON"
+fi
+
 if [ ! -d "Build" ]; then
 	mkdir Build
 fi
 
 cd Build
 
-if [ "$1" == "COVERAGE" ]; then
-cmake ../ -G "Unix Makefiles" -DCOVERAGE=ON
-else
-cmake ../ -G "Unix Makefiles"
-fi
+cmake ../ -G "Unix Makefiles" ${Arguments}
 make
