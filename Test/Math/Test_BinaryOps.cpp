c5f01fc4a3a5d63b0f5eb4ca77288598e7e305a0
XenonicDev <andrewdepke@gmail.com>
2018-03-01 18:45:58 -0700

    Added New BinaryOps Unit Tests

--- Test/Math/Test_BinaryOps.cpp
+++ Test/Math/Test_BinaryOps.cpp
@@ -147,3 +147,79 @@ TEST(BinaryOpsSuite, BitFlipUnsigned)
 
 	EXPECT_EQ(Expected, Sample);
 }
+
+TEST(BinaryOpsSuite, BitsSetGeneral)
+{
+	// Big Endian: 10011111011001100001011110111010
+
+#if PLATFORM_BIGENDIAN
+	int32_t Sample = -1620699206;
+#else
+	int32_t Sample = 1575511801;
+#endif
+
+	EXPECT_EQ(19, BitsSet(Sample));
+}
+
+TEST(BinaryOpsSuite, NoBitsSet)
+{
+	// Big Endian: 00000000000000000000000000000000
+
+	int32_t Sample = 0;
+
+	EXPECT_EQ(0, BitsSet(Sample));
+}
+
+TEST(BinaryOpsSuite, AllBitsSet)
+{
+	// Big Endian: 11111111111111111111111111111111
+
+	int32_t Sample = ~0;
+
+	EXPECT_EQ(32, BitsSet(Sample));
+}
+
+TEST(BinaryOpsSuite, GetSignGeneral)
+{
+	int SampleA = 3;
+	int SampleB = -2;
+	int SampleC = 0;
+
+	EXPECT_EQ(1, GetSign(SampleA));
+	EXPECT_EQ(-1, GetSign(SampleB));
+	EXPECT_EQ(0, GetSign(SampleC));
+}
+
+TEST(BinaryOpsSuite, InverseSignsGeneral)
+{
+	int SampleA = 3;
+	int SampleB = -2;
+	int SampleC = 0;
+
+	EXPECT_EQ(false, InverseSigns(SampleA, SampleA));
+	EXPECT_EQ(true, InverseSigns(SampleA, SampleB));
+	EXPECT_EQ(true, InverseSigns(SampleB, SampleC));
+	EXPECT_EQ(false, InverseSigns(SampleA, SampleC));
+}
+
+TEST(BinaryOpsSuite, IsPowerOf2General)
+{
+	unsigned int SampleA = 16;
+	unsigned int SampleB = 3;
+	unsigned int SampleC = 0;
+
+	EXPECT_EQ(true, IsPowerOf2(SampleA));
+	EXPECT_EQ(false, IsPowerOf2(SampleB));
+	EXPECT_EQ(false, IsPowerOf2(SampleC));
+}
+
+TEST(BinaryOpsSuite, RoundUpToPowerOf2General)
+{
+	unsigned int SampleA = 16;
+	unsigned int SampleB = 3;
+	unsigned int SampleC = 0;
+
+	EXPECT_EQ(16, RoundUpToPowerOf2(SampleA));
+	EXPECT_EQ(4, RoundUpToPowerOf2(SampleB));
+	EXPECT_EQ(0, RoundUpToPowerOf2(SampleC));
+}
