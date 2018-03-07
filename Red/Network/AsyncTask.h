b1f1c75f4ebe50f07fd8385c3aeeb34bc30fc16d
XenonicDev <andrewdepke@gmail.com>
2018-03-07 14:36:44 -0700

    Adding to AsyncTask

--- Red/Network/AsyncTask.h
+++ Red/Network/AsyncTask.h
@@ -5,25 +5,38 @@
 
 namespace Red
 {
+	// Used to Manipulate and Query an Asynchronous Function
 	class AsyncTask
 	{
 	protected:
-		std::future<void(void)> Handle;
+		std::future<void(void)>* Handle;
 
 	public:
+		AsyncTask(std::future<void(void)>* InHandle) : Handle(InHandle) {}
+
 		void WaitForCompletion()
 		{
-			Handle.get();
+			Handle->get();
 		}
 
 		bool IsFinished()
 		{
-			return Handle.wait_for(std::chrono::seconds(0)) == std::future_status::ready;
+			return Handle->wait_for(std::chrono::seconds(0)) == std::future_status::ready;
 		}
 
 		void Abort()
 		{
 
+		}
+
+		void Cleanup()
+		{
+			if (Handle)
+			{
+				delete Handle;
+
+				Handle = nullptr;
+			}
 		}
 	};
 }  // namespace Red
