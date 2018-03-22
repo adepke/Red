a2daf4dc5746d298a2e292608c4f1606f69ba6e5
XenonicDev <andrewdepke@gmail.com>
2018-03-22 17:29:17 -0600

    Added Process Base Header

---
+++ Red/Debug/Process.h
@@ -0,0 +1,20 @@
+/*
+	Copyright (c) 2017-2018 Andrew Depke
+*/
+#pragma once
+
+#include "../Core/Platform.h"
+
+#if OS_WINDOWS
+	#include "Windows/WindowsProcess.h"
+#elif OS_MACINTOSH
+	#include "Linux/LinuxProcess.h"
+#elif OS_LINUX
+	#include "Linux/LinuxProcess.h"
+#elif OS_BSD
+	#include "Linux/LinuxProcess.h"
+#elif OS_ANDROID
+	#include "Linux/LinuxProcess.h"
+#elif OS_IOS
+	#include "Linux/LinuxProcess.h"
+#endif
