a2c2d81b070c0ef2648e9c91d093f790ea8f5a86
XenonicDev <andrewdepke@gmail.com>
2018-03-22 19:25:13 -0600

    Added Darwin Process Implementations

---
+++ Red/Debug/Darwin/DarwinProcess.cpp
@@ -0,0 +1,12 @@
+/*
+	Copyright (c) 2017-2018 Andrew Depke
+*/
+#include "DarwinProcess.h"
+
+namespace Red
+{
+	bool GetProcessModules(std::vector<ProcessModule>* Output)
+	{
+		return true;  // Not Yet Implemented on Darwin Platforms
+	}
+}  // namespace Red
