9d1e2f1a3cc34460a3b34bd55cfbfb90514eed29
Andrew Depke <andrewdepke@gmail.com>
2018-03-25 01:39:01 -0600

    Fixed Unused Return Value Warning

--- Red/Hardware/Linux/LinuxHardware.cpp
+++ Red/Hardware/Linux/LinuxHardware.cpp
@@ -136,15 +136,16 @@ namespace Red
 
 	unsigned long int LinuxSystemHardware::GetDiskSpaceAvailable()
 	{
-		char Directory[1024];
+		char Directory[PATH_MAX];
 
-		getcwd(Directory, sizeof(Directory));
-
-		struct statfs FileSystemStats = { 0 };
-
-		if (statfs(Directory, &FileSystemStats) == 0)
+		if (getcwd(Directory, sizeof(Directory)))
 		{
-			return (FileSystemStats.f_bavail * FileSystemStats.f_bsize / 1024);
+			struct statfs FileSystemStats = { 0 };
+
+			if (statfs(Directory, &FileSystemStats) == 0)
+			{
+				return (FileSystemStats.f_bavail * FileSystemStats.f_bsize / 1024);
+			}
 		}
 
 		return 0;
