f1e92bf6e98cf5e3d99f7262c9136e02bbee51db
Andrew Depke <andrewdepke@gmail.com>
2018-03-26 20:13:23 -0600

    Implemented Basic Darwin GetProcessModules()

--- Red/Debug/Darwin/DarwinProcess.cpp
+++ Red/Debug/Darwin/DarwinProcess.cpp
@@ -3,10 +3,30 @@
 */
 #include "DarwinProcess.h"
 
+#include <mach-o/dyld.h>
+#include <string.h>
+
 namespace Red
 {
 	bool GetProcessModules(std::vector<ProcessModule>* Output)
 	{
-		return true;  // Not Yet Implemented on Darwin Platforms
+		unsigned int ModuleCount = _dyld_image_count();
+		if (ModuleCount == 0)
+		{
+			return false;
+		}
+
+		for (unsigned int Iter = 0; Iter < ModuleCount; ++Iter)
+		{
+			ProcessModule Module;
+
+			Module.Name = _dyld_get_image_name(Iter);
+			Module.BaseAddress = "";
+			Module.FileSize = 0;
+
+			Output->push_back(Module);
+		}
+
+		return true;
 	}
 }  // namespace Red
