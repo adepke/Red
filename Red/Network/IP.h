6fe55fc4b9ecb78b8ef7d180884cecff2e618174
Andrew Depke <andrewdepke@gmail.com>
2018-03-27 19:12:01 -0600

    Added Parent IP Functions

--- Red/Network/IP.h
+++ Red/Network/IP.h
@@ -7,11 +7,25 @@ namespace Red
 {
 	struct IPAddress
 	{
+	public:
 		virtual ~IPAddress() = 0;
+
+	public:
+		virtual bool IsLoopbackAddress() const = 0;
+		virtual bool IsMulticastAddress() const = 0;
+
+		virtual operator std::string() const = 0;
 	};
 
 	struct IPEndPoint
 	{
+	public:
 		virtual ~IPEndPoint() = 0;
+
+	public:
+		virtual bool operator==(const IPEndPoint& Target) const = 0;
+		virtual bool operator!=(const IPEndPoint& Target) const = 0;
+
+		virtual operator std::string() const = 0;
 	};
 }  // namespace Red
