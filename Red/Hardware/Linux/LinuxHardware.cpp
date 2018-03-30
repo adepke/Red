cc08d483b89a3dfbfd84a476ad800bf0b80722c1
Andrew Depke <andrewdepke@gmail.com>
2018-03-29 20:19:18 -0600

    Added Linux Hardware Lazy Fetching

--- Red/Hardware/Linux/LinuxHardware.cpp
+++ Red/Hardware/Linux/LinuxHardware.cpp
@@ -37,117 +37,149 @@ namespace Red
 
 	std::string LinuxSystemHardware::GetCPUVendor()
 	{
-		unsigned int Data[4] = { 0 };
-		char Vendor[13] = { 0 };
-
-		if (__get_cpuid(0, &Data[0], &Data[1], &Data[2], &Data[3]) != 0)
+		static std::string CPUVendor = NULL;
+		if (CPUVendor.length() == 0)
 		{
-			// CPU vendor is stored in EBX EDX ECX (ASCII)
-			std::memcpy(Vendor + 0, &Data[1], 4);  // EBX
-			std::memcpy(Vendor + 4, &Data[3], 4);  // ECX
-			std::memcpy(Vendor + 8, &Data[2], 4);  // EDX
+			unsigned int Data[4] = { 0 };
+			char Vendor[13] = { 0 };
+
+			if (__get_cpuid(0, &Data[0], &Data[1], &Data[2], &Data[3]) != 0)
+			{
+				// CPU vendor is stored in EBX EDX ECX (ASCII)
+				std::memcpy(Vendor + 0, &Data[1], 4);  // EBX
+				std::memcpy(Vendor + 4, &Data[3], 4);  // ECX
+				std::memcpy(Vendor + 8, &Data[2], 4);  // EDX
+			}
+
+			else
+			{
+				return "Error";
+			}
+
+			Vendor[12] = '\0';
+
+			CPUVendor = Vendor;
 		}
 
-		else
-		{
-			return "Error";
-		}
-
-		Vendor[12] = '\0';
-
-		return Vendor;
+		return CPUVendor;
 	}
 
 	std::string LinuxSystemHardware::GetCPUBrand()
 	{
-		unsigned int Data[4] = { 0 };
-		char Brand[49] = { 0 };
-
-		if (__get_cpuid(0x80000002, &Data[0], &Data[1], &Data[2], &Data[3]) != 0)
+		static std::string CPUBrand = NULL;
+		if (CPUBrand.length() == 0)
 		{
-			std::memcpy(Brand + 0, &Data[0], 4);
-			std::memcpy(Brand + 4, &Data[1], 4);
-			std::memcpy(Brand + 8, &Data[2], 4);
-			std::memcpy(Brand + 12, &Data[3], 4);
+			unsigned int Data[4] = { 0 };
+			char Brand[49] = { 0 };
+
+			if (__get_cpuid(0x80000002, &Data[0], &Data[1], &Data[2], &Data[3]) != 0)
+			{
+				std::memcpy(Brand + 0, &Data[0], 4);
+				std::memcpy(Brand + 4, &Data[1], 4);
+				std::memcpy(Brand + 8, &Data[2], 4);
+				std::memcpy(Brand + 12, &Data[3], 4);
+			}
+
+			else
+			{
+				return "Error";
+			}
+
+			if (__get_cpuid(0x80000003, &Data[0], &Data[1], &Data[2], &Data[3]) != 0)
+			{
+				std::memcpy(Brand + 16, &Data[0], 4);
+				std::memcpy(Brand + 20, &Data[1], 4);
+				std::memcpy(Brand + 24, &Data[2], 4);
+				std::memcpy(Brand + 28, &Data[3], 4);
+			}
+
+			else
+			{
+				return "Error";
+			}
+
+			if (__get_cpuid(0x80000004, &Data[0], &Data[1], &Data[2], &Data[3]) != 0)
+			{
+				std::memcpy(Brand + 32, &Data[0], 4);
+				std::memcpy(Brand + 36, &Data[1], 4);
+				std::memcpy(Brand + 40, &Data[2], 4);
+				std::memcpy(Brand + 44, &Data[3], 4);
+			}
+
+			else
+			{
+				return "Error";
+			}
+
+			Brand[48] = '\0';
+
+			CPUBrand = Brand;
 		}
 
-		else
-		{
-			return "Error";
-		}
-
-		if (__get_cpuid(0x80000003, &Data[0], &Data[1], &Data[2], &Data[3]) != 0)
-		{
-			std::memcpy(Brand + 16, &Data[0], 4);
-			std::memcpy(Brand + 20, &Data[1], 4);
-			std::memcpy(Brand + 24, &Data[2], 4);
-			std::memcpy(Brand + 28, &Data[3], 4);
-		}
-
-		else
-		{
-			return "Error";
-		}
-
-		if (__get_cpuid(0x80000004, &Data[0], &Data[1], &Data[2], &Data[3]) != 0)
-		{
-			std::memcpy(Brand + 32, &Data[0], 4);
-			std::memcpy(Brand + 36, &Data[1], 4);
-			std::memcpy(Brand + 40, &Data[2], 4);
-			std::memcpy(Brand + 44, &Data[3], 4);
-		}
-
-		else
-		{
-			return "Error";
-		}
-
-		Brand[48] = '\0';
-
-		return Brand;
+		return CPUBrand;
 	}
 
 	unsigned int LinuxSystemHardware::GetCPUCoreCount()
 	{
-		return std::thread::hardware_concurrency();
+		static unsigned int CPUCoreCount = NULL;
+		if (CPUCoreCount == 0)
+		{
+			CPUCoreCount = std::thread::hardware_concurrency();
+		}
+
+		return CPUCoreCount;
 	}
 
 	unsigned long int LinuxSystemHardware::GetPhysicalMemory()
 	{
-		return (sysconf(_SC_PHYS_PAGES) * sysconf(_SC_PAGE_SIZE));
+		static unsigned long int PhysicalMemory = NULL;
+		if (PhysicalMemory == 0)
+		{
+			PhysicalMemory = (sysconf(_SC_PHYS_PAGES) * sysconf(_SC_PAGE_SIZE));
+		}
+
+		return PhysicalMemory;
 	}
 
 	unsigned long int LinuxSystemHardware::GetDiskSpace()
 	{
-		char Directory[1024];
-
-		if (getcwd(Directory, sizeof(Directory)))
+		static unsigned long int DiskSpace = NULL;
+		if (DiskSpace == 0)
 		{
-			struct statfs FileSystemStats = { 0 };
+			char Directory[1024];
 
-			if (statfs(Directory, &FileSystemStats) == 0)
+			if (getcwd(Directory, sizeof(Directory)))
 			{
-				return (FileSystemStats.f_blocks * FileSystemStats.f_bsize / 1024);
+				struct statfs FileSystemStats = { 0 };
+
+				if (statfs(Directory, &FileSystemStats) == 0)
+				{
+					DiskSpace = (FileSystemStats.f_blocks * FileSystemStats.f_bsize / 1024);
+				}
 			}
 		}
 
-		return 0;
+		return DiskSpace;
 	}
 
 	unsigned long int LinuxSystemHardware::GetDiskSpaceAvailable()
 	{
-		char Directory[1024];
-
-		if (getcwd(Directory, sizeof(Directory)))
+		static unsigned long int DiskSpaceAvailable = NULL;
+		if (DiskSpaceAvailable == 0)
 		{
-			struct statfs FileSystemStats = { 0 };
+			char Directory[1024];
 
-			if (statfs(Directory, &FileSystemStats) == 0)
+			if (getcwd(Directory, sizeof(Directory)))
 			{
-				return (FileSystemStats.f_bavail * FileSystemStats.f_bsize / 1024);
+				struct statfs FileSystemStats = { 0 };
+
+				if (statfs(Directory, &FileSystemStats) == 0)
+				{
+					DiskSpaceAvailable = (FileSystemStats.f_bavail * FileSystemStats.f_bsize / 1024);
+				}
 			}
 		}
 
-		return 0;
+		return DiskSpaceAvailable;
 	}
 }  // namespace Red
